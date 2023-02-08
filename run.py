a3eea15d7b53a7feabff01a6db0a534c6fa8e74a
Renzo Nogueira <mainrenzossn@gmail.com>
2023-02-08 00:43:02 -0300

    reun.py atualizado, na chamada agora é possível passar argumentos, como a chave e o nome de usuário.

--- run.py
+++ run.py
@@ -1,37 +1,101 @@
 ## importar arquivo functions.py da pasta functions
-import sys
-sys.path.append('config')
-from settings import *
-sys.path.append('utils')
-from utils import *
+import openai
+import argparse
+import os
+import webbrowser
+from train import *
 
-respostas = ""
-while True:
-    # Limpa o console
-    prompt = input("Você: ")
-    # Verifica se o comando é para criar uma imagem
-    if prompt == "criar imagem":
-        # exibir lista 1: '256x256', 2: '512x512', 3: '1024x1024'
-        tamanhos = {
-            "1": "256x256",
-            "2": "512x512",
-            "3": "1024x1024",
-        }
-        for key, value in tamanhos.items():
-            print(key,":", value)
-        size = input("Tamanho da imagem: ")
-        size = tamanhos[size]
-        promptImage = input("Descrição da imagem: ")
-        createImage(promptImage, size)
-        continue
-    response = creatCompletion(model, prompt, temperature, max_tokens, train, respostas)
-    
-    # Verifica se no texto contem chaves
-    if "{" in response:
-        # Pega o texto entre chaves
-        comand = response.split("{")[1].split("}")[0]
-        # Executa o comando
-        executeCommand(comand)
+os.environ["PYTHONIOENCODING"] = "utf-8"
+model = "text-davinci-003"
+temperature=0
+max_tokens=300
+key = "..."
+nameUser = "User"
 
-    respostas += response
-    print(response)
+parser = argparse.ArgumentParser() # cria um objeto para receber os argumentos
+## Chave da API
+parser.add_argument("-k", "--key", help="Sua chave da OpenAI.", type=str)
+## O Nome do usuário
+parser.add_argument("-n", "--user", help="O seu nome.", type=str)
+
+def main():
+
+    ## FUNÇÃO PARA CRIAR IMAGEM
+    def createImage(prompt, size="1024x1024"):
+        response = openai.Image.create(
+        prompt=prompt,
+        n=1,
+        size=size,
+        )
+        response = response['data'][0]['url']
+        # Abre a imagem criada no navegador
+        webbrowser.open(response)
+
+    ## Função executar chat
+    def creatCompletion(model, prompt, temperature, max_tokens, train, respostas):
+        response = openai.Completion.create(
+            model=model,
+            prompt=train + respostas + "Você: " + prompt,
+            temperature=temperature,
+            max_tokens=max_tokens
+        )
+        response = response["choices"][0]["text"]
+        return response
+
+    ## FUNÇÃO PARA EXECUTAR COMANDOS
+    def executeCommand(command):
+        os.system(command)
+
+    ## FUNÇÃO PARA LIMPAR O CONSOLE
+    def clearConsole():
+        os.system("cls")
+
+
+    ## EXECUTAR O CHAT ##
+    respostas = ""
+    while True:
+        # Limpa o console
+        prompt = input("Você: ")
+        # Verifica se o comando é para criar uma imagem
+        if prompt == "criar imagem":
+            # exibir lista 1: '256x256', 2: '512x512', 3: '1024x1024'
+            tamanhos = {
+                "1": "256x256",
+                "2": "512x512",
+                "3": "1024x1024",
+            }
+            for key, value in tamanhos.items():
+                print(key,":", value)
+            size = input("Tamanho da imagem: ")
+            size = tamanhos[size]
+            promptImage = input("Descrição da imagem: ")
+            createImage(promptImage, size)
+            continue
+        response = creatCompletion(model, prompt, temperature, max_tokens, train, respostas)
+        
+        # Verifica se no texto contem chaves
+        if "{" in response:
+            # Pega o texto entre chaves
+            comand = response.split("{")[1].split("}")[0]
+            # Executa o comando
+            executeCommand(comand)
+
+        respostas += response
+        print(response)
+
+## Executa a função main() se o arquivo for executado diretamente
+if __name__ == "__main__":
+    args = parser.parse_args()
+    if args.key:
+        key = args.key
+        print("Chave de API redefinida", openai.api_key)
+    if args.user:
+        nameUser = args.user
+        print("Novo usuário definido: " + nameUser)
+    openai.api_key = key ## Chave da API
+    ## Configura o treinamento
+    train = train.replace("{nameUser}", nameUser).replace("{cunstomTrain}", cunstomTrain.replace("{nameUser}", nameUser))
+    ## Imprime a ajuda sobre os argumentos
+    parser.print_help()
+    print("........Chat iniciazado........")
+    main()
---
+++ train.py
@@ -0,0 +1,16 @@
+cunstomTrain = """Olá, me chame de {nameUser}, sou programador e desenvolvedor Web, nasci em quatro de junho de dois mil e um, tenho 21 anos, moro em Brasília."""
+
+train = """
+{cunstomTrain}
+Caso eu diga "execute" você mostrará comandos CMD Windows 11, ao contrário, responda a minha pergunta normalmente. qualquer comando que você listar deverá ser entre chaves, exemplo {comando}, o meu usuário do sistema é desenvolvimento01.
+Siga o exemplo de chat abaixo:
+{nameUser}: Olá chat.<|endoftext|>
+Chat: {comando} Olá {nameUser}. Como possso te ajudar?<|endoftext|>
+{nameUser}: Execute listar arquivos do desktop.<|endoftext|>
+Chat: {dir C:\\Users\\desenvolvimento01\\Desktop} Aqui está a saída do comando.<|endoftext|>
+{nameUser}: Execute clima.<|endoftext|>
+Chat: {curl wttr.in?lang=pt} Aqui está o clima.<|endoftext|>
+{nameUser}: Tem como um cnpj terminar com 00?<|endoftext|>
+Chat: Sim, um CNPJ pode terminar com 00. Isso acontece porque o CNPJ é composto por 14 dígitos, sendo os dois últimos dígitos um dígito verificador. O dígito verificador é calculado com base nos outros 12 dígitos, e se o resultado do cálculo for 0, então o CNPJ terminará com 00.<|endoftext|>
+"""
+# train = train.replace("{nameUser}", yourName).replace("{cunstomTrain}", cunstomTrain)
